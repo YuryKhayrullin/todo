927b5f52afa50683ad1039b423489fb602fe6c8f
YuryKhayrullin <hiru19871@yandex.ru>
2025-04-14 14:31:53 +0300

    second commit

--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function TodoList ({todo, setTodo}) {
+
+  const [edit, setEdit] = useState(null)
 
   function deleteTodo (id) {     {/*получает id*/}
 let newTodo = [...todo].filter(item => item.id!=id) //перебераемый id не должен быть равен id получаемый из кнопки delete
@@ -16,15 +18,28 @@ function statusTodo (id) {
   })
   setTodo(newTodo)
 }
+function editTodo (id) {
+setEdit(true)
+}
   return (
 <div> 
-  {todo.map( item =>
-  <div key={item.id}>
+  {
+    todo.map( item => (
+      <div key={item.id}>
+    {
+      edit ? 
+      <div>
+        <input/>
+        <button>Сохранить</button>
+        </div> :
+        <div>{ item.title }</div>
+    }
     <div>{item.title}</div> {/*вывели значение title из todo State*/}
   <button onClick={ () => deleteTodo(item.id)}>Удалить</button>
+  <button onClick={ () => editTodo(item.id)}>Редактировать</button>
   <button onClick={ () => statusTodo(item.id)}>Закрыть / Открыть</button>
   </div>
-  )}
+  ))}
 </div>
   )
 }
